6a5c56110437effd63062bc73e73e532c21373f0
rofi <rfigueroa@truelogic.io>
2023-08-30 11:27:18 -0400

    import fixes

--- app/(tabs)/account/index.tsx
+++ app/(tabs)/account/index.tsx
@@ -1,5 +1,5 @@
 import { AccountScreen } from 'modules/account/account-screen'
-import AccountLayout from "app/modules/account/templates/account-layout";
+import AccountLayout from "modules/account/templates/account-layout";
 
 export default function AccountPage() {
   return <AccountLayout><AccountScreen /></AccountLayout>
--- modules/home/screen.tsx
+++ modules/home/screen.tsx
@@ -4,6 +4,7 @@ import Hero from './components/hero'
 import FeaturedProducts from 'modules/home/components/featured-products'
 
 export function HomeScreen() {
+  
   return (
     <ScrollView className={'bg-white'}>
       <Hero />
